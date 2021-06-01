1703f9588771f07caddbcf1fd3c162204bdde1f1
Lyric Doshi <lyric@mit.edu>
2021-05-31 22:48:20 -0700

    Implement support for varying number of gaps of varying widths, along with varying heights and widths for ground components. Add a new value to differentiate state locations not occupied, occupied by ground, or occupied by bricks. Update render artwork.

--- gym_bridges/envs/bridges_env.py
+++ gym_bridges/envs/bridges_env.py
@@ -6,6 +6,13 @@ from gym.utils import seeding
 
 import numpy as np
 from random import choice
+from random import randrange
+from typing import NamedTuple
+
+class InitialBlock(NamedTuple):
+  index: int
+  width: int
+  height: int
 
 class BridgesEnv(gym.Env):
   metadata = {'render.modes': ['human']}
@@ -13,46 +20,31 @@ class BridgesEnv(gym.Env):
   def __init__(self):
     pass
 
-  # vary_heights will uniformly vary the starting heights on the left
-  # and right side of the gap. In the current implementation, a bridge
-  # will always be possible.
-  def setup(self, height, width, vary_heights=False):
-    self.shape = (height, width)
+  # The max height of the ground of either end will be [1, width).
+  # The max height of the env will be 1.5 times the width.
+  # In the current implementation, a bridge will always be possible.
+  def setup(self, width, max_gap_count=1, force_standard_config=False):
+    assert width > max_gap_count * 2, "The max gap count must be less than half the width"
+    self.shape = (int(1.5 * width), width)
 
     self.nA = width
     self.action_space = spaces.Discrete(self.nA)
+    self.max_gap_count = max_gap_count
+    self.force_standard_config = force_standard_config
 
     self.start = (self.shape[0]-1, 0)
     self.end = (self.shape[0]-1, self.shape[1]-1)
-
-    self.height_pairs = []
-
-    start_height_max = 1
-    if vary_heights:
-      start_height_max = height
-      
-    for start_height in range(start_height_max):
-      for end_height in range(start_height + 1):
-        if self._check_feasible(start_height, end_height):
-          self.height_pairs.append((start_height, end_height))
-          if start_height != end_height:
-            self.height_pairs.append((end_height, start_height))
           
     self.reset()
  
     self.brick = 2
-
-  # Verify if the maximum possible span before hitting the height
-  # limit is sufficient to bridge the gap.
-  def _check_feasible(self, start_height, end_height):
-    return (start_height - end_height) + 2 * (self.shape[0] - 1 - start_height) >= self.shape[1]-2
     
   def _check_row(self, action, index, brick_width):
     section = self.state[index][action:action+brick_width]
     return len(section) == brick_width and not section.any()
 
   def _place_brick(self, action, index, brick_width):
-    self.state[index][action:action+brick_width] = 1
+    self.state[index][action:action+brick_width] = 2
 
   def _is_bridge_complete(self):
     # Quick check.
@@ -116,37 +108,66 @@ class BridgesEnv(gym.Env):
       done = True
         
     return self.state.copy(),reward,done,{}
-      
-  def reset(self, state=None, height_pair=None):
-    if state is None:
-      self.state = np.zeros(shape=self.shape)
-
-      if height_pair is None:
-        height_pair = choice(self.height_pairs)
-      for start_adjustment in range(height_pair[0] + 1):
-        self.state[self.start[0] - start_adjustment][self.start[1]] = 1
-      for end_adjustment in range(height_pair[1] + 1):
-        self.state[self.end[0] - end_adjustment][self.end[1]] = 1
-    else:
+  
+  def reset(self, state=None, gap_count=None):
+    if state is not None:
       self.state = state.copy()
+      return self.state.copy()
+
+    self.state = np.zeros(shape=self.shape)
+
+    if not gap_count:
+      gap_count = randrange(1, self.max_gap_count + 1)
+      
+    initial_blocks = []
+
+    index = self.start[1]
+    for i in range(gap_count+1):
+      if gap_count:
+        # We must ensure we leave enough room for the remaining gaps
+        # and blocks at the minimum width of 1.
+        width = 1 + randrange(self.shape[1] - index - 2 * gap_count)
+      else:
+        # The final block must go until the end of the environment.
+        width = self.shape[1] - index
+
+      height = randrange(1, self.shape[1])
+      initial_blocks.append(InitialBlock(index, width, height))
+
+      gap_count -= 1
+      if gap_count < 0:
+        break
+
+      # Build a random size gap that ensures we still have room for
+      # the remaining gaps and blocks at the minimum width of 1.
+      index = index + width + 1 + randrange(self.shape[1] - (index + width + 1) - (2 * gap_count))
+
+    if self.force_standard_config:
+      initial_blocks = [
+        InitialBlock(index=self.start[1],width=1,height=1),
+        InitialBlock(index=self.end[1],width=1,height=1)
+      ]
+      
+    for initial_block in initial_blocks:
+      for env_index in range(initial_block.index, initial_block.index + initial_block.width):
+        for height_adjustment in range(initial_block.height):
+          self.state[self.start[0]-height_adjustment][env_index] = 1
 
     return self.state.copy()
       
   def render(self, mode='human'):
-    print((("%s"*self.shape[1]+"\n")*self.shape[0]) % tuple(["X" if x else " " for x in self.state.flatten()]))
+    print((("%s"*self.shape[1]+"\n")*self.shape[0]) % tuple(["@@" if x == 1 else "[]" if x == 2 else "  " for x in self.state.flatten()]))
       
   def close(self):
     pass
 
-env = BridgesEnv()
-env.setup(3,5, vary_heights=True)
-env.reset()
+#env = BridgesEnv()
+#env.setup(9, 2)
 
-#for _ in range(100):
+#for _ in range(40):
 #    observation, reward, done, _ = env.step(env.action_space.sample())
 #    env.render()
 #    if done:
 #      print ("DONE")
 #      break
-
 #env.close()
---
+++ gym_bridges/envs/bridges_env_test.py
@@ -0,0 +1,280 @@
+import numpy as np
+import pytest
+import unittest
+
+from collections import defaultdict
+from parameterized import parameterized
+
+from bridges_env import BridgesEnv
+
+def _check_block(state, index, width_check_value):
+    """Verifies that the block start at index is grounded in the environment and maintains a level height until the next gap. Returns the index following this block on success. Gaps are checked as blocks of height 0."""
+    env_height = state.shape[0] - 1 
+    block_height = 0
+
+    while state[env_height - block_height][index] == 1:
+        block_height += 1
+
+    i = index + 1
+    while i < state.shape[1] and state[env_height][i] == width_check_value:
+        i_height = 0
+        while state[env_height - i_height][i] == 1:
+            i_height += 1
+            
+        if i_height != block_height:
+            return False
+
+        i += 1
+
+    if state[:,index:i].sum() != block_height * (i - index):
+        return False
+
+    return i
+
+def _state_builder(shape, heights):
+    state = np.zeros(shape)
+    state_height = state.shape[0]
+    for i, height in enumerate(heights):
+        state[state_height - height:state_height, i] = 1
+    return state        
+
+def _step_helper(action, env, width, mirror=False):
+    if mirror:
+       action = width - 2 - action
+       
+    env.step(action)
+
+def _reset_helper(heights, env, mirror=False):
+    if mirror:
+        heights.reverse()
+        
+    env.reset(state=_state_builder(env.shape, heights))
+
+class TestBridgesEnv(unittest.TestCase):
+    def setUp(self):
+        self.env = BridgesEnv()
+
+    @parameterized.expand(
+        [
+            (3,),
+            (9,)
+        ]
+    )
+    def test_force_standard_config(self, width):
+        """The standard configuration only has a value of 1 at the bottom left and right locations of the env."""
+        self.env.setup(width=width, force_standard_config=True)
+        height = self.env.shape[0] -1 
+        self.assertEqual(self.env.state[height][0], 1)
+        self.assertEqual(self.env.state[height][width-1], 1)
+        self.assertEqual(self.env.state.sum(), 2)
+
+    @parameterized.expand(
+        [
+            (3,),
+            (9,)
+        ]
+    )
+    def test_gap_count(self, width):
+        """Verifies the requested number of gaps exist. This test also verifies that the env is well-formed in terms of having initial blocks of ground (1 values) properly grounded to the bottom of the env and level between gaps."""        
+        self.env.setup(width=width)
+        for gap_count in range(1, ((width + 1) // 2)):
+            self.env.reset(gap_count=gap_count)
+            # Verify gap columns are completely empty.
+            index = 0
+            width_check_value = 1
+            counter = 0 
+            while index < width:
+                index = _check_block(self.env.state, index, width_check_value)
+                self.assertTrue(index)
+                    
+                width_check_value = (width_check_value + 1) % 2
+                # Misses the first value so this should equal the gap count.
+                counter += width_check_value 
+
+            self.assertEqual(counter, gap_count)
+
+
+    @parameterized.expand(
+        [
+            (3,),
+            (9,)
+        ]
+    )
+    def test_max_gap_count(self, width):
+        """Verifies the gap count varies between 1 and the max gap count."""        
+        for gap_count in range(1, ((width + 1) // 2)):
+            self.env.setup(width=width, max_gap_count=gap_count)
+            height = self.env.shape[0] -1 
+            counts = defaultdict(lambda: 0)
+            for _ in range(100):
+                self.env.reset()
+                counts[int(np.sum([self.env.state[height][i] if self.env.state[height][i] - self.env.state[height][i+1] else 0 for i in range(self.env.state.shape[1] -1)]))] += 1
+
+            self.assertEqual(len(counts), gap_count)
+
+    def test_is_bridge_complete_simple(self):
+        """Verifies bridge completion detection in a very simple set-up."""
+        self.env.setup(width=4)
+        self.assertFalse(self.env._is_bridge_complete())
+        self.env.step(0)
+        self.assertFalse(self.env._is_bridge_complete())
+        self.env.step(0)
+        self.assertFalse(self.env._is_bridge_complete())
+        self.env.step(2)
+        self.assertTrue(self.env._is_bridge_complete())
+
+    @parameterized.expand(
+        [
+            (False,),
+            (True,)
+        ]
+    )        
+    def test_is_bridge_complete_side_completion(self, mirror):
+        """Verifies bridge completion detection when the bridge is completed via a side connection to an endpoint instead being placed on top of one of the endpoints."""
+
+        width = 3
+        self.env.setup(width=width)
+
+        # Side completion to starting point.
+        _reset_helper([2,0,1], self.env, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(1, self.env, width, mirror)
+        self.assertTrue(self.env._is_bridge_complete())
+
+    @parameterized.expand(
+        [
+            (False,),
+            (True,)
+        ]
+    )        
+    def test_is_bridge_complete_wide_endpoint(self, mirror):
+        """Verifies bridge completion detection when the bridge completes by touching any section of a wide endpoint."""
+
+        width = 5
+        self.env.setup(width=width)
+
+        # Test two completions with a wide starting point.
+        _reset_helper([1,1,1,0,1], self.env, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(3, self.env, width, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(1, self.env, width, mirror)
+        self.assertTrue(self.env._is_bridge_complete())
+
+        _reset_helper([1,1,1,0,1], self.env, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(3, self.env, width, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(0, self.env, width, mirror)
+        _step_helper(2, self.env, width, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(0, self.env, width, mirror)
+        self.assertTrue(self.env._is_bridge_complete())
+
+#        TODO(lyric) add render to each test case!
+    @parameterized.expand(
+        [
+            (False,),
+            (True,)
+        ]
+    )        
+    def test_is_bridge_complete_cliff_side_insufficient(self, mirror):
+        """Verifies bridge completion detection when the bridge touches the side of a cliff on one end, but not the top."""
+
+        width = 3
+        self.env.setup(width=width)
+
+        _reset_helper([1,0,3], self.env, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(0, self.env, width, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(0, self.env, width, mirror)
+        self.assertTrue(self.env._is_bridge_complete())
+
+    @parameterized.expand(
+        [
+            (False,),
+            (True,)
+        ]
+    )        
+    def test_is_bridge_complete_center_ground_surface(self, mirror):
+        """Verifies bridge completion detection when the bridge touches a central ground component that's not part of the left or the right. Crossing over central ground is permitted as part of connecting the left and right. This test verifies the case where bridges adjoin the center ground at surface height."""
+
+        width = 6
+        self.env.setup(width=width)
+
+        _reset_helper([1,0,2,2,0,1], self.env, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(0, self.env, width, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(4, self.env, width, mirror)
+        self.env.render()
+        self.assertTrue(self.env._is_bridge_complete())
+
+    @parameterized.expand(
+        [
+            (False,),
+            (True,)
+        ]
+    )        
+    def test_is_bridge_complete_center_ground_build_up(self, mirror):
+        """Verifies bridge completion detection when the bridge touches a central ground component that's not part of the left or the right. Crossing over central ground is permitted as part of connecting the left and right. This test verifies the case where bridges connect to the center ground by building up from it."""
+
+        width = 9
+        self.env.setup(width=width)
+
+        _reset_helper([1,0,0,1,1,1,0,0,1], self.env, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(0, self.env, width, mirror)
+        _step_helper(7, self.env, width, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(2, self.env, width, mirror)
+        _step_helper(5, self.env, width, mirror)
+        self.env.render()
+        self.assertTrue(self.env._is_bridge_complete())
+        
+    @parameterized.expand(
+        [
+            (False,),
+            (True,)
+        ]
+    )        
+    def test_is_bridge_complete_center_ground_surface_and_build_up(self, mirror):
+        """Verifies bridge completion detection when the bridge touches a central ground component that's not part of the left or the right. Crossing over central ground is permitted as part of connecting the left and right. This test verifies the case where one bridge connects to the center ground by building up from it and the other adjoins the center ground at surface height."""
+
+        width = 8
+        self.env.setup(width=width)
+
+        _reset_helper([1,0,2,2,2,0,0,2], self.env, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(0, self.env, width, mirror)
+        _step_helper(6, self.env, width, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(4, self.env, width, mirror)
+        self.env.render()
+        self.assertTrue(self.env._is_bridge_complete())
+
+    @parameterized.expand(
+        [
+            (False,),
+            (True,)
+        ]
+    )        
+    def test_is_bridge_complete_skip_center_ground(self, mirror):
+        """Verifies bridge completion detection when the bridge avoids touching a central ground component that's not part of the left or the right. Touching central ground is not required."""
+
+        width = 6
+        self.env.setup(width=width)
+
+        _reset_helper([1,0,1,1,0,1], self.env, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(0, self.env, width, mirror)
+        _step_helper(1, self.env, width, mirror)
+        _step_helper(4, self.env, width, mirror)
+        self.assertFalse(self.env._is_bridge_complete())
+        _step_helper(3, self.env, width, mirror)
+        self.assertTrue(self.env._is_bridge_complete())
+
+if __name__ == "__main__":
+    unittest.main()
+
